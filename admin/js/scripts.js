e9fcc99669708e01e11c4626513198f4c5a6bfef
Dinko <dinko.dugec@gmail.com>
2022-04-13 00:02:28 +0200

    Pulling User Id with jQuery and Javascript

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+  var user_href;
+  var user_href_splitted;
+  var user_id;
+
+  $(".modal_thumbnails").click(function () {
+    $("#set_user_image").prop("disabled", false);
+
+    user_href = $("#user-id").prop("href");
+    user_href_splitted = user_href.split("=");
+    user_id = user_href_splitted[user_href_splitted.length - 1];
+
+    alert(user_id);
+  });
+
+  tinymce.init({ selector: "textarea" });
+});
+
+$(document).ready(function () {
   $("#summernote").summernote({
     height: 300,
   });
