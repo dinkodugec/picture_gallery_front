b895a8962ff6c1082c5cedf51446105d772e8c94
Dinko <dinko.dugec@gmail.com>
2022-04-13 00:08:33 +0200

    pulling image name with jQuery and JS

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   var user_href;
   var user_href_splitted;
   var user_id;
+  var image_src;
+  var image_href_splitted;
+  var image_id;
 
   $(".modal_thumbnails").click(function () {
     $("#set_user_image").prop("disabled", false);
@@ -10,7 +13,11 @@ $(document).ready(function () {
     user_href_splitted = user_href.split("=");
     user_id = user_href_splitted[user_href_splitted.length - 1];
 
-    alert(user_id);
+    image_src = $(this).prop("src");
+    image_href_splitted = image_src.split("/");
+    image_id = image_href_splitted[image_href_splitted.length - 1];
+
+    alert(image_id);
   });
 
   tinymce.init({ selector: "textarea" });
