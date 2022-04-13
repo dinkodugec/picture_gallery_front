f0401e8f60d46272ed1ce613599d1958158ed390
Dinko <dinko.dugec@gmail.com>
2022-04-13 21:56:53 +0200

    jQuery and AJAX

---
+++ admin/includes/ajax_code.php
@@ -0,0 +1,9 @@
+<?php require("init.php");
+     
+   if(isset($_POST['image_name'])){
+
+    echo "This is data from the server YEH";
+   }
+
+
+?>
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   var user_id;
   var image_src;
   var image_href_splitted;
-  var image_id;
+  var image_name;
 
   $(".modal_thumbnails").click(function () {
     $("#set_user_image").prop("disabled", false);
@@ -15,9 +15,23 @@ $(document).ready(function () {
 
     image_src = $(this).prop("src");
     image_href_splitted = image_src.split("/");
-    image_id = image_href_splitted[image_href_splitted.length - 1];
+    image_name = image_href_splitted[image_href_splitted.length - 1];
 
-    alert(image_id);
+    /* alert(image_name); */
+  });
+
+  $("#set_user_image").click(function () {
+    $.ajax({
+      url: "includes/ajax_code.php",
+      data: { image_name: image_name, user_id: user_id },
+      type: "POST",
+      success: function (data) {
+        if (!data.error) {
+          alert(data);
+        }
+      },
+    });
+    // alert(image_name);  it works here without ajax
   });
 
   tinymce.init({ selector: "textarea" });
