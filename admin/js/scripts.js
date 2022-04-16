046d8cb155537dbaf0c7e114bbcaeb94f0c92769
Dinko <dinko.dugec@gmail.com>
2022-04-16 18:30:28 +0200

    Photo library Sidebar

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
   var image_src;
   var image_href_splitted;
   var image_name;
+  var photo_id;
 
   $(".modal_thumbnails").click(function () {
     $("#set_user_image").prop("disabled", false);
@@ -16,6 +17,19 @@ $(document).ready(function () {
     image_src = $(this).prop("src");
     image_href_splitted = image_src.split("/");
     image_name = image_href_splitted[image_href_splitted.length - 1];
+
+    photo_id = $(this).attr("data");
+
+    $.ajax({
+      url: "icludes/ajax_code.php",
+      data: { photo_id: photo_id },
+      type: "POST",
+      success: function (data) {
+        if (!data.error) {
+          $("#modal_sidebar").html(data);
+        }
+      },
+    });
 
     /* alert(image_name); */
   });
